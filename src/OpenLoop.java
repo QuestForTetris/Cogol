6cfea875b57ac67e1d8fcc1e9814e83fc8b7b802
PhiNotPi <PhiNotPi@users.noreply.github.com>
2016-06-14 00:29:21 -0400

    Update OpenLoop.java

--- src/OpenLoop.java
+++ src/OpenLoop.java
@@ -1,25 +1,27 @@
 import java.util.ArrayList;
 
-
 public class OpenLoop {
+
+  static int nextLoopID = 0;
+
   String type;
   int id;
   String name;
   ArrayList<Command> commands;
 
-  public OpenLoop(String type, int id) {
+  public OpenLoop(String type) {
     super();
     this.type = type;
-    this.id = id;
+    this.id = nextLoopID++;
     this.name = "";
     this.commands = new ArrayList<Command>();
   }
-  
-  public String toString(){
+
+  public String toString() {
     return capitalize(type) + id + name;
   }
-  
+
   private String capitalize(final String line) {
     return Character.toUpperCase(line.charAt(0)) + line.substring(1);
- }
+  }
 }
