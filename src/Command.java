a420b99d85401663f54654e80a93ee7209ed0141
PhiNotPi <PhiNotPi@users.noreply.github.com>
2016-06-14 00:28:53 -0400

    Update Command.java

--- src/Command.java
+++ src/Command.java
@@ -31,15 +31,33 @@ public class Command {
       arg2 = arg2.dup();
       arg2.val *= -1;
     }
-    if(isSymmetric()){
-      if(arg1.mode < arg2.mode){
+    if (isSymmetric()) {
+      if (arg1.mode < arg2.mode) {
         Arg temp = arg1;
         arg1 = arg2;
         arg2 = temp;
-      } else if (arg1.val < arg2.val){
+      } else if (arg1.mode == arg2.mode && arg1.val < arg2.val) {
         Arg temp = arg1;
         arg1 = arg2;
         arg2 = temp;
+      }
+    }
+    if (opcode.equals("MLZ") && arg1.mode == 0) {
+      if (arg1.val < 0) {
+        arg1.val = -1;
+      } else {
+        arg1.val = 0;
+        arg2 = new Arg(0);
+        arg3 = new Arg(0);
+      }
+    }
+    if (opcode.equals("MNZ") && arg1.mode == 0) {
+      opcode = "MLZ";
+      if (arg1.val != 0) {
+        arg1.val = -1;
+      } else {
+        arg2 = new Arg(0);
+        arg3 = new Arg(0);
       }
     }
   }
